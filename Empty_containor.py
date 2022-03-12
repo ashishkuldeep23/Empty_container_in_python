dedba724343c13983a1b3be2e38c27f787cb1f76
Ashishkuldeep06 <72973524+Ashishkuldeep06@users.noreply.github.com>
2022-03-12 19:22:49 +0530

    Ashish
    This code for Python containers.
    This tells how many containers present in python and their empty forms.
    Thank you.

---
+++ Empty_containor.py
@@ -0,0 +1,21 @@
+''' Try to make empty Containors that is:
+List
+Tuple
+Dictonary
+Set
+'''
+# Empty List
+l=[]
+print("l is",type(l))
+
+# Empty Tuple
+t=( )
+print("t is",type(t))
+
+# Empty Dictonary
+d={ }
+print("d is",type(d))
+
+# Empty Set
+s=set( )
+print("s is",type(s))
